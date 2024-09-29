300c30d98d219d70932d2ca564a98aadeacab715
Sergio <principiante.en.programar@gmail.com>
2024-09-29 12:49:56 -0300

    fix issue with image x6

--- Componentes/Setting/setting.jsx
+++ Componentes/Setting/setting.jsx
@@ -121,7 +121,6 @@ export default function Settings() {
 
                 const data = cld
                     .image(data.public_id);
-
                 const image = data.toURL();
                 if (image != "") {
                     const response = await fetch(image);
